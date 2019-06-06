4747a39788263d9340b333c419bbf143210cfc25
Ali <syed.hussain@qantas.com.au>
2019-06-06 22:51:14 +1000

    Singleton thread list

--- src/Singleton_Pattern/Singleton.java
+++ src/Singleton_Pattern/Singleton.java
@@ -1,17 +1,40 @@
 package Singleton_Pattern;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Singleton {
 
 	private static Singleton firstInstance = null;
+
+	String[] scrabbleLetters = {"a", "a", "a", "a", "a", "b", "b", "b", "b",
+								"c", "c", "c", "c", "d", "d", "d", "d", "e",
+								"f", "f", "f", "f", "f", "g", "g", "g", "g"};
+
+	private LinkedList<String> letterList = new LinkedList<>(Arrays.asList(scrabbleLetters));
+
+	static boolean firstThread = true;
+
 
 	private Singleton() {}
 
 	public Singleton getFirstInstance() {
 		if (firstInstance == null) {
+
+			if (firstThread) {
+				firstThread = false;
+
+				Thread.currentThread();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 			firstInstance = new Singleton();
 		}
 		return firstInstance;
 	}
 
-	
+
 }
